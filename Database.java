c56a7ba6b09bfdb22c185c1a1ee7bb44abb57412
kfitz98 <37519487+kfitz98@users.noreply.github.com>
2018-04-16 01:24:54 +0100

    Update Database.java

--- Database.java
+++ Database.java
@@ -2,9 +2,8 @@ package assignment;
 
 import java.sql.*;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
+
 
 public class Database {
 	
@@ -17,6 +16,7 @@ public class Database {
 	public Database() {
 		
 		try {
+			//Establish connect to database
 			String driver = "com.mysql.jdbc.Driver";
 			String url = "jdbc:mysql://localhost:3306/Assignment";
 			String username = "root";
@@ -25,22 +25,15 @@ public class Database {
 			
 			Connection conn = DriverManager.getConnection(url,username,password);
 			st = conn.createStatement();
-			
-		}catch(Exception e) {System.out.println(e);}
+			//
+		}catch(Exception e) {System.out.println("Error: Failed to connect");}
 		
 	}
 	
-	//take data from database
-	
-//	public void Data() {
-//		try {
-//			result = st.executeQuery("SELECT MIN(Passed) FROM nct");
-//			System.out.println(result.getInt(0));
-//			
-//
-//			
-//			
-//		}catch(Exception e) {}
-//	}
+	public void Data() {
+		
+
+	}
+
 	
 }
